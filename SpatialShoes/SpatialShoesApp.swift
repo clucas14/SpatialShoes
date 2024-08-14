bf190c9234d29fa8b21b3d155f709b8fccec3fea
Carlos Lucas <lukolukas@icloud.com>
2024-08-14 15:36:59 +0200

    Initial Commit

---
+++ SpatialShoes/SpatialShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialShoesApp.swift
+//  SpatialShoes
+//
+//  Created by Carlos Lucas Sánchez on 14/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
